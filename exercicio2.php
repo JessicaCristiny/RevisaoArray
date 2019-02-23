cedb2d298bf5391be5cbb462169ff465301b6d7d
jessi <jessi@LAPTOP-8JVJIOAK>
2019-02-23 13:10:38 -0300

    comitt exercicio2

---
+++ exercicio2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>exercicio2</title>
+    </head>
+    <body>
+        <?php
+        
+        for($i=0; $i <=50; $i++){
+            $vetor50[$i] = ($i);
+            echo $vetor50 [$i]."\n";
+        }
+        
+     
+        print_r($vetor50);
+        ?>
+    </body>
+</html>
